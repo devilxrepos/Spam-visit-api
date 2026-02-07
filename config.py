b6ef55bbd9b1efe92bf338feae905382a8b01832
devilxrepos <su939789@gmail.com>
2026-02-07 13:04:10 +0600

    Add files via upload

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+MAX_USAGE=50
+DB_NAME = "spam_xpert"
+MONGO_URI = os.getenv("MONGO_URI") 
+BASE_URLS = {
+    "IND": "https://client.ind.freefiremobile.com",
+    "BR":  "https://client.us.freefiremobile.com",
+    "ME":  "https://clientbp.ggblueshark.com",
+    "BD":  "https://clientbp.ggblueshark.com",
+    "PK":  "https://clientbp.ggblueshark.com",
+    "EUROPE":"https://clientbp.ggblueshark.com",
+
+     "VN":"https://clientbp.ggblueshark.com",
+     "SG":"https://clientbp.ggblueshark.com",
+}
+
+REGION_CONFIG = {
+    region: {
+        "tokens":    f"{region.lower()}_tokens",
+        "url_spam":  f"{base}/RequestAddingFriend",
+        "url_visit": f"{base}/GetPlayerPersonalShow",
+    }
+    for region, base in BASE_URLS.items()
+}
+
+
+
